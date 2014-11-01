8cba7d3cc9925ceb88cd2b3fe3ab737d47ccd22c
Sirawesomeknight <hulsizerjames@gmail.com>
2014-11-01 17:04:09 -0500

    MainPage
    -Styled Main Page
    -Added Menu Bar
    -Added Main Title
    -Changed Font
    -Added About Button
    -Added PROTOTYPE IFrames

--- JS/mainpage.js
+++ JS/mainpage.js
@@ -2,3 +2,6 @@ function load(){
 var getuser = window.localStorage.getItem("mySharedData");
 document.getElementById("welback").innerHTML = "Welcome back "+getuser;
 }
+function about(){
+alert("Made By: James Hulsizer in Affiliation with the Francis W. Parker School Version 1");
+}
