13d82b38d55e3d4f4b34da83c98136136be774c4
Sirawesomeknight <hulsizerjames@gmail.com>
2014-11-25 22:20:50 -0600

    Version 1.0.4
    -Added more adjustments to packages.js

--- Prototype V2/Javascript/packages.js
+++ Prototype V2/Javascript/packages.js
@@ -29,17 +29,35 @@ parent.removeChild(child);
 parent.removeChild(child2);
 parent.removeChild(child3);
 parent.removeChild(child4);
-var list = document.createElement("ul");
-var listcomp = document.createElement("li");
-var titles = document.createElement("p");
-var mainstring = ["Version 1.1","Gets Notifications","Gets Due Dates"];
-titles.innerHTML = "EDMODO Features:";
-titles.style.color = "white";
-listcomp.style.color = "white";
-parent.appendChild(titles);
-parent.appendChild(list);
-for(i = 0; i < mainstring.length; i++){
-listcomp.innerHTML = mainstring[i];
-list.appendChild(listcomp);
+var list = document.createElement("p");
+var list2 = document.createElement("p");
+var list3 = document.createElement("p");
+var title1 = document.createElement("p");
+var title2 = document.createElement("p");
+var title3 = document.createElement("p");
+var bstring = "-";
+var edmodostring = ["Version 1.1","Gets Notifications","Gets Due Dates"];
+var schlstring = ["Version 1.1","Gets Notifications","Gets Due Dates","Gets Tests"];
+var googstring = ["Version 1.0","Displays IFrame"];
+title1.innerHTML = "EDMODO Features:";
+title2.innerHTML = "SCHOOLOGY Features:";
+title3.innerHTML = "GOOGLE CLASSROOM Features:";
+title1.style.color = "white";
+title2.style.color = "white";
+title3.style.color = "white";
+parent.appendChild(title1);
+for(i = 0; i < edmodostring.length; i++){
+list.innerHTML += bstring + edmodostring[i] + "<br>";
 }
+parent.appendChild(list);
+parent.appendChild(title2);
+for(i = 0; i < schlstring.length; i++){
+list2.innerHTML += bstring + schlstring[i] + "<br>";
+}
+parent.appendChild(list2);
+parent.appendChild(title3);
+for(i = 0; i < googstring.length; i++){
+list3.innerHTML += bstring + googstring[i] + "<br>";
+}
+parent.appendChild(list3);
 }
